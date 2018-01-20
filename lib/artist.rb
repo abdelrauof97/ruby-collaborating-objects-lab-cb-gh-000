4b133c694c829943973451ba83868f3bd717a10e
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-20 02:33:05 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,50 @@
+class Artist
 
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+  
+  def self.find_by_name(name)
+    self.all.detect { |x| x.name = name } 
+  end
+
+  def self.create_by_name(name)
+    artist = self.new(name) 
+    self.all << artist 
+    artist
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def songs
+    @songs
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+  
+  def print_songs
+    @songs.each { |x| puts x.name }
+  end
+
+end
 
 class Artist
   attr_accessor :name, :songs
