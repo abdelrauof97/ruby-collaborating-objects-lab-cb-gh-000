ba08c9eea2d1f883e087f635af70230ecd0c58ae
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-20 02:10:46 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,7 +8,7 @@ class MP3Importer
   end
   
   def files 
-    @files = Dir.glob("#{@path}/*.mp3").collect{ |x| x.gsup{"#{path}", ""}
+    @files = Dir.glob("#{@path}/*.mp3").collect{ |x| x.gsup{/#{path}/, ""}
   end
   
   
