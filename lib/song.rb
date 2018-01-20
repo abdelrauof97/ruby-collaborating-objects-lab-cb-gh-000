6c674b0b65f3c539c35825253b5a30b31ae2a352
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-20 01:05:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,13 +5,6 @@ class Song
   def initialize(name)
     @name = name
   end
-  
-  def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
-  end
 
   
 end
