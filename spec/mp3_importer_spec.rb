7f1c1c7505bcc95c1969b1e5edc8a8921a835ea0
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-20 02:25:56 +0000

    automatically backed up by learn

--- spec/mp3_importer_spec.rb
+++ spec/mp3_importer_spec.rb
@@ -33,7 +33,7 @@ describe "MP3Importer" do
   describe '#import' do 
     it 'imports the files into the library by creating songs from a filename' do
       test_music_path = "./spec/fixtures/mp3s"
-      
+      Artist.class_variable_set("@@all",[])
       music_importer = MP3Importer.new(test_music_path)
       music_importer.import
 
