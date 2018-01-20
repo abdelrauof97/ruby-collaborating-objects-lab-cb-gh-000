21a11be24485ab0d0affbabfbe72d664a2176af4
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-20 00:38:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,6 +13,8 @@ class Song
   end
   
 end
-
+      new_instance = Song.new_by_filename(file_name)
+      expect(new_instance.name).to eq('Black or White')
+      expect(new_instance.artist.name).to eq('Michael Jackson')
 
 # learn spec/song_spec.rb --fail-fast
