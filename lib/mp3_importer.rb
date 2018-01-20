df67324dc69f9da10881985936afd3d76ec487c6
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-20 02:30:16 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,19 @@
 
+class MP3Importer
+  attr_accessor :path
 
+  def initialize(music_path)
+    @path = music_path
+  end
+
+  def files
+      @files ||= Dir.glob("#{path}/*.mp3").collect{|f| f.gsub("#{path}/", "")}
+  end
+
+  def import
+      files.each{|f| Song.new_by_filename(f)}
+  end
+end
 
 
 
