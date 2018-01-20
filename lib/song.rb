08384e9c370bcccc8b4b14ae04fc67ee61054452
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-20 00:34:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,8 @@ class Song
   
   def new_by_filename(filename)
     new_file = filename.split(" - ")
-    self.name = new_file[]
-    self.artist = new_file[]
+    self.name = new_file[2]
+    self.artist = new_file[1]
   end
   
 end
